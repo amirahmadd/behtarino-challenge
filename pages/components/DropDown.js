22a89d3f556bfc8ec3bcaecf9acb8beb8777f56e
kalantari <kalantari@parsdata.com>
2022-09-05 20:16:46 +0430

    add select box in product options

---
+++ pages/components/DropDown.js
@@ -0,0 +1,40 @@
+import { Select, MenuItem } from "@mui/material";
+import { useState } from "react";
+import style from "./DropDown.module.css";
+import { KeyboardArrowDown } from "@mui/icons-material";
+
+const DropDown = (props) => {
+  const { options, title } = props;
+  const [value, setValue] = useState(options[0]?.id);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <Select
+        labelId={`${title}-select`}
+        id={`${title}-select`}
+        value={value}
+        label={title}
+        className={style.select}
+        variant="standard"
+        onChange={handleChange}
+        IconComponent={(props) => (
+          <i {...props} className={`material-icons ${props.className} ${style.icon}`}>
+            <KeyboardArrowDown />
+          </i>
+        )}
+      >
+        {options.map((item) => (
+          <MenuItem key={item.id} value={item.id}>
+            ({item.title})
+          </MenuItem>
+        ))}
+      </Select>
+    </div>
+  );
+};
+
+export default DropDown;
--- pages/components/ProductOptions.js
+++ pages/components/ProductOptions.js
@@ -1,13 +1,30 @@
 import { Divider, Grid } from "@mui/material";
 import { useState } from "react";
+import DropDown from "./DropDown";
 import style from "./ProductOptions.module.css";
 
-const colors = ["#63f3c9", "#ffff72", "#ff7a76", "#d6e2e1", "black"];
+const productColors = [
+  { id: 1, color: "#63f3c9" },
+  { id: 2, color: "#ffff72" },
+  { id: 3, color: "#ff7a76" },
+  { id: 4, color: "#d6e2e1" },
+  { id: 5, color: "black" },
+];
+const productSize = [
+  { id: 1, title: "UK 7" },
+  { id: 2, title: "UK 8" },
+  { id: 3, title: "UK 9" },
+];
+const productAvailableCount = [
+  { id: 1, title: 1 },
+  { id: 2, title: 2 },
+  { id: 3, title: 3 },
+];
 
 const ProductOptions = () => {
-  const [selected, setSelected] = useState(colors[0]);
+  const [selected, setSelected] = useState(productColors[0].id);
   const clickHandler = (color) => {
-    setSelected(color);
+    setSelected(color.id);
   };
   return (
     <div className={style.optionContainer}>
@@ -15,18 +32,18 @@ const ProductOptions = () => {
         <Grid item md={6} xs={12}>
           <div className={style.colorTitle}>COLOR</div>
           <div className={style.colorSelect}>
-            {colors.map((color) => (
+            {productColors.map((color) => (
               <span
-                key={color}
+                key={color.id}
                 className={
-                  selected === color ? style.ActiveColorBall : style.noBorder
+                  selected === color.id ? style.ActiveColorBall : style.noBorder
                 }
-                style={{ borderColor: color }}
+                style={{ borderColor: color.color }}
               >
                 <span
                   className={style.colorBall}
                   //   selected === color ? style.ActiveColorBall :
-                  style={{ backgroundColor: color }}
+                  style={{ backgroundColor: color.color }}
                   onClick={() => clickHandler(color)}
                 />
               </span>
@@ -35,11 +52,15 @@ const ProductOptions = () => {
         </Grid>
         <Grid item md={3} xs={6}>
           <div className={style.optionTitle}>SIZE</div>
-          <div className={style.WithBorderOption}>size</div>
+          <div className={style.WithBorderOption}>
+            <DropDown title="SIZE" options={productSize} />
+          </div>
         </Grid>
         <Grid item md={3} xs={6}>
-          <div className={style.optionTitle}>COUNT</div>
-          <div className={style.option}>count</div>
+          <div className={style.optionTitle}>QTY</div>
+          <div className={style.option}>
+            <DropDown title="QTY" options={productAvailableCount} />
+          </div>
         </Grid>
       </Grid>
     </div>
